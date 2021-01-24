46a4f186363b2c377aa15e1e72f68dd1481448e0
Kyle Shankwiler <kyleshankwiler@gmail.com>
2021-01-24 13:55:52 -0800

    initial

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const nonsenseFunction = () => 0;
 
 export default (req, res) => {
+  throw new Error('test');
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
